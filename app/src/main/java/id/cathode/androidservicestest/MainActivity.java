4eef982665e2710fc646224ca95dea0a13c2ee1a
yusufatw <yusufatw@gmail.com>
2018-06-21 04:10:22 +0700

    Add MyService.java

--- app/src/main/java/id/cathode/androidservicestest/MainActivity.java
+++ app/src/main/java/id/cathode/androidservicestest/MainActivity.java
@@ -1,13 +1,27 @@
 package id.cathode.androidservicestest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Android ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: is running");
+    }
+
+    public void startService(View view){
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view){
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 }
